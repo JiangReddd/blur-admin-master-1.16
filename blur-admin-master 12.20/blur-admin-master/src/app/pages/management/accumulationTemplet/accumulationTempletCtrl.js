67283a11f590f3fb10e218462ea5f9fcf4432e4d
JiangReddd <824041451@qq.com>
2017-02-10 10:09:38 +0800

    勋章积分模板上传下载接口对接完成

--- blur-admin-master 12.20/blur-admin-master/src/app/pages/management/accumulationTemplet/accumulationTempletCtrl.js
+++ blur-admin-master 12.20/blur-admin-master/src/app/pages/management/accumulationTemplet/accumulationTempletCtrl.js
@@ -9,9 +9,46 @@
       .controller('accumulationTempletCtrl', accumulationTempletCtrl);
 
   /** @ngInject */
-  function accumulationTempletCtrl($scope, $timeout) {
+  function accumulationTempletCtrl($scope, $timeout,$http,$window) {
+  	  	$scope.uploadAccumulation = function(){
+  		var data = new FormData();
+        var file = document.querySelector('input[type=file]').files[0];
+        //var file2 = $scope.myFile;
+        console.log(file);
+        //console.log(file2);
+        data.append('file', file);
+        var url = "/admin/uploadCreditFile"; 
+
+        $http({
+              method:'POST',
+              url: url,
+              data: data,
+              headers: {'Content-Type':undefined},
+               })   
+              .success( function(response)
+                       {
+                       		//上传成功的操作
+                       		console.log("上传成功");
+                       })
+              .error(function(response){
+              				console.log("上传失败");
+              				alert("上传失败");
+              })
+  		/*$http.post(url,data)
+  		.success(function(res){
+
+  		})
+  		.error(function(res){
+
+  		})*/
+  	}
+
+  	$scope.downloadFile = function(){
+  		$window.location = "/admin/downloadCreditModel";
+  	}
+
     $scope.progressFunction = function() {
-      return $timeout(function() {}, 3000);
+      return $timeout(function() {}, 300);
     };
   }
 
--- blur-admin-master 12.20/blur-admin-master/src/app/pages/management/medalTemplet/medalTempletCtrl.js
+++ blur-admin-master 12.20/blur-admin-master/src/app/pages/management/medalTemplet/medalTempletCtrl.js
@@ -9,10 +9,45 @@
       .controller('medalTempletCtrl', medalTempletCtrl);
 
   /** @ngInject */
-  function medalTempletCtrl($scope, $timeout) {
-    $scope.progressFunction = function() {
-      return $timeout(function() {}, 3000);
-    };
+  function medalTempletCtrl($scope, $timeout,$http,$window) {
+  	$scope.uploadMedal = function(){
+  		var fd = new FormData();
+        var file = document.querySelector('input[type=file]').files[0];
+        //var file2 = $scope.myFile;
+        console.log(file);
+        //console.log(file2);
+        data.append('file', file);
+        var url = "/admin/uploadMedalFile"; 
+
+        $http({
+              method:'POST',
+              url: url,
+              data: data,
+              headers: {'Content-Type':undefined},
+               })   
+              .success(function(response)
+                       {
+                       		//上传成功的操作
+                       		console.log("上传成功");
+                       })
+              .error(function(response){
+              				console.log("上传失败");
+              				alert("上传失败");
+              })
+  		/*$http.post(url,data)
+  		.success(function(res){
+
+  		})
+  		.error(function(res){
+
+  		})*/
+  	}
+
+  	$scope.downloadFile = function(){
+  		$window.location = "/admin/downloadMedalModel";
+  	}
+
+    
   }
 
 })();
